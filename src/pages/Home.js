64d9f28bdedfca6fec83e578059739a24eb3e193
neel123go <ayounpaul1029@gmail.com>
2022-12-05 14:01:44 +0600

    Redux Acc Moon tech project completed with useReducre & context api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
+  const { state: { products, error, loading } } = useProducts();
+  let content;
+
+  if (loading) {
+    content = <p>Loading..</p>
+  }
+
+  if (error) {
+    content = <p>Something went wrong!</p>
+  }
+
+  if (!loading && !error && products.length === 0) {
+    content = <p>Sorry, All products are already sale.</p>
+  }
+
+  if (!loading && !error && products.length) {
+    content = products.map(product => <ProductCard key={product._id} product={product} />);
+  }
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h2>This is home page</h2>
+      {content}
     </div>
   );
 };
